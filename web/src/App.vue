0562e7673aff394b9967bae29888d239361229c5
zheng.wang <zheng.Wang@mobiledrivetech.com>
2023-10-23 15:19:13 +0800

    add Vant resolver

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { Tabbar, TabbarItem } from 'vant'
+import { Button } from 'vant'
 </script>
 
 <template>
   <RouterView />
   <div>
+    <Button>111111</Button>
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
     </nav>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search" dot>标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o" badge="5">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" badge="20">标签</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
