f8a4132b557469a023e4f8a94d625d464f6ba295
zheng.wang <zheng.Wang@mobiledrivetech.com>
2023-10-24 16:07:02 +0800

    add wing-vue-viewport v0.1.0

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,10 +4,10 @@ import { RouterView } from 'vue-router'
 
 <template>
   <RouterView />
-  <van-tabbar route>
-    <van-tabbar-item icon="home-o" replace to="/">首页</van-tabbar-item>
-    <van-tabbar-item icon="search" dot replace to="/course">课程</van-tabbar-item>
-    <van-tabbar-item icon="friends-o" badge="5" replace to="/news">资讯</van-tabbar-item>
-    <van-tabbar-item icon="setting-o" badge="20" replace to="/mine">我的</van-tabbar-item>
-  </van-tabbar>
+  <VanTabbar route>
+    <VanTabbarItem icon="home-o" replace to="/">首页</VanTabbarItem>
+    <VanTabbarItem icon="search" dot replace to="/course">课程</VanTabbarItem>
+    <VanTabbarItem icon="friends-o" badge="5" replace to="/news">资讯</VanTabbarItem>
+    <VanTabbarItem icon="setting-o" badge="20" replace to="/mine">我的</VanTabbarItem>
+  </VanTabbar>
 </template>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useViewportWidthStore } from 'wing-vue-viewport'
+const viewportWidthStore = useViewportWidthStore()
+const { isMobile, width, isMediumViewPort, isExtraLargeViewPort } = storeToRefs(viewportWidthStore)
+</script>
+
 <template>
   <h1>Home</h1>
+  <h1>width --{{ width }}</h1>
+  <h1>isMobile --{{ isMobile }}</h1>
+  <h1>isMediumViewPort --{{ isMediumViewPort }}</h1>
+  <h1>isExtraLargeViewPort --{{ isExtraLargeViewPort }}</h1>
 </template>
