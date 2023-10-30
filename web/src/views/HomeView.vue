7501125883af5f0e579cb0bfb995e09d00a7a9b1
zheng.wang <zheng.Wang@mobiledrivetech.com>
2023-10-30 18:00:26 +0800

    update calendar

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,38 +1,46 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+const minDate: Date = new Date(2020, 1, 1)
+const showCalendar: boolean = ref(false)
+const showTimePicker: boolean = ref(false)
+let tempDateTime: string = ref('')
+let dates: date[] = ref([
+  new Date('2023/10/23 11:00:00'),
+  new Date('2023/10/23 15:30:00'),
+  new Date('2023/10/24 15:30:00'),
+  new Date('2023/10/27 15:30:00')
+])
 
-const dateTime = ref('')
-const showCalendar = ref(false)
-const showTimePicker = ref(false)
-let dates = [new Date('2023-10-26'), new Date('2023-10-27')]
-
-const onCalendarConfirm = (value) => {
-  // dateTime.value = ''
-  // showCalendar.value = false
-  // dateTime.value += `${value.getFullYear()}-${value.getMonth() + 1}-${value.getDate()}`
-  // showTimePicker.value = true
-  console.log(value, value[0], value[1].toLocaleString())
-  dates.push(value)
-  d = value
+const onCalendarConfirm = (date: Date) => {
+  showCalendar.value = false
+  showTimePicker.value = true
+  tempDateTime.value = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
 }
 const onTimePickerConfirm = ({ selectedValues }) => {
   showTimePicker.value = false
-  dateTime.value += ` ${selectedValues.join(':')}`
+  tempDateTime.value += ` ${selectedValues.join(':')}`
+
+  const preDates = [...dates.value, new Date(tempDateTime.value)]
+  dates.value = preDates
 }
-
 const formatter = (day: Day) => {
-  const month = day.date.getMonth() + 1
-  const date = day.date.getDate()
-
-  if (month === 10) {
-    if (date === 26) {
-      console.log(1111111, day)
-    } else if (date === 27) {
-      console.log(22222, day)
-    }
+  if (
+    day.type.indexOf('selected') > -1 ||
+    day.type.indexOf('start') > -1 ||
+    day.type.indexOf('end') > -1
+  ) {
+    console.log(1111111, day.type)
+  } else {
+    day.type = 'disabled' // middle
   }
-
   return day
+}
+function select(v) {
+  console.log('select', v)
+}
+function unselect(v) {
+  console.log('unselect', v)
+  return false
 }
 </script>
 <template>
@@ -43,15 +51,22 @@ const formatter = (day: Day) => {
       style="height: 70vh"
       :poppable="false"
       :show-confirm="false"
-      :default-date="dates"
       :formatter="formatter"
-      :min-date="new Date('2020-1-1')"
+      :min-date="minDate"
+      :max-date="new Date()"
+      :default-date="dates"
+      @select="select"
+      @unselect="unselect"
     />
-    <h1>{{ dateTime }}</h1>
     <div class="punching-btn">
       <VanButton type="primary" block @click="showCalendar = true">打卡</VanButton>
     </div>
-    <VanCalendar v-model:show="showCalendar" @confirm="onCalendarConfirm" />
+    <VanCalendar
+      v-model:show="showCalendar"
+      @confirm="onCalendarConfirm"
+      :min-date="minDate"
+      :max-date="new Date()"
+    />
     <VanPopup v-model:show="showTimePicker" round position="bottom" style="height: 50%">
       <VanTimePicker
         title="选择时间"
