525a3805fa113171be02fe3be99c63aa234ec263
zheng.wang <zheng.Wang@mobiledrivetech.com>
2023-11-08 17:58:37 +0800

    add record detail page

---
+++ web/src/views/Record/RecordDetail.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+<template>
+  <div>
+    <VanNavBar
+      :title="`记录详细-${router.params.id}`"
+      left-text="返回"
+      left-arrow
+      @click-left="() => $router.back()"
+    />
+    {{ router.params.id }}
+  </div>
+</template>
